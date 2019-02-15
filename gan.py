f448cf3b63165e169fca47d9fff18302a5dae31b
Ilias Bachiri <ilias.bachiri@gmail.com>
2019-02-15 19:57:28 +0100

    Added names to all layers

--- gan.py
+++ gan.py
@@ -11,173 +11,173 @@ from tensorflow.python.saved_model import tag_constants
 from tensorflow.python.tools import inspect_checkpoint as chkp
 
 
-def encoder(image, usage=''):
-    with tf.variable_scope('encoder', reuse=tf.AUTO_REUSE) as scope:
+def encoder(image):
+    with tf.variable_scope('encoder', reuse=True) as scope:
         # Input Layer
-        input_layer = tf.reshape(image, [-1, 128, 128, 3])
+        input_layer = tf.reshape(image, [-1, 128, 128, 3], 'e_input')
 
         # Convolution Layer 1
         w_conv1 = tf.get_variable('e_wconv1', [6, 6, 3, 64])
         b_conv1 = tf.get_variable('e_bconv1', [64])
-        h_conv1 = tf.nn.relu(conv2d(input_layer, w_conv1, strides=[1, 1, 1, 1], padding='SAME') + b_conv1)
+        h_conv1 = tf.nn.relu(conv2d(input_layer, w_conv1, strides=[1, 1, 1, 1], padding='SAME') + b_conv1, 'e_relu1')
 
         # Pooling Layer 1
-        h_pool_1 = tf.layers.max_pooling2d(inputs=h_conv1, pool_size=[2, 2], strides=2)
+        h_pool_1 = tf.layers.max_pooling2d(inputs=h_conv1, pool_size=[2, 2], strides=2, name='e_pool1')
 
         # Convolution Layer 2
         w_conv2 = tf.get_variable('e_wconv2', [4, 4, 64, 128])
         b_conv2 = tf.get_variable('e_bconv2', [128])
-        h_conv2 = tf.nn.relu(conv2d(h_pool_1, w_conv2, strides=[1, 1, 1, 1], padding='SAME') + b_conv2)
+        h_conv2 = tf.nn.relu(conv2d(h_pool_1, w_conv2, strides=[1, 1, 1, 1], padding='SAME') + b_conv2, name='e_relu2')
 
         # Pooling Layer 2
-        h_pool_2 = tf.layers.max_pooling2d(inputs=h_conv2, pool_size=[2, 2], strides=2)
+        h_pool_2 = tf.layers.max_pooling2d(inputs=h_conv2, pool_size=[2, 2], strides=2, name='e_pool2')
 
         # Convolution Layer 3
         w_conv3 = tf.get_variable('e_wconv3', [4, 4, 128, 256])
         b_conv3 = tf.get_variable('e_bconv3', [256])
-        h_conv3 = tf.nn.relu(conv2d(h_pool_2, w_conv3, strides=[1, 1, 1, 1], padding='SAME') + b_conv3)
+        h_conv3 = tf.nn.relu(conv2d(h_pool_2, w_conv3, strides=[1, 1, 1, 1], padding='SAME') + b_conv3, name='e_relu3')
 
         # Pooling Layer 3
-        h_pool_3 = tf.layers.max_pooling2d(inputs=h_conv3, pool_size=[2, 2], strides=2)
+        h_pool_3 = tf.layers.max_pooling2d(inputs=h_conv3, pool_size=[2, 2], strides=2, name='e_pool3')
 
         # Convolution Layer 4
         w_conv4 = tf.get_variable('e_wconv4', [4, 4, 256, 256])
         b_conv4 = tf.get_variable('e_bconv4', [256])
-        h_conv4 = tf.nn.relu(conv2d(h_pool_3, w_conv4, strides=[1, 1, 1, 1], padding='SAME') + b_conv4)
+        h_conv4 = tf.nn.relu(conv2d(h_pool_3, w_conv4, strides=[1, 1, 1, 1], padding='SAME') + b_conv4, name='e_relu4')
 
         # Pooling Layer 4
-        h_pool_4 = tf.layers.max_pooling2d(inputs=h_conv4, pool_size=[2, 2], strides=2)
+        h_pool_4 = tf.layers.max_pooling2d(inputs=h_conv4, pool_size=[2, 2], strides=2, name='e_pool4')
 
         # Fully connected layer 1
         w_fc_1 = tf.get_variable('e_wfc1', [8 * 8 * 256, 256])
         b_fc_1 = tf.get_variable('e_bfc1', [256])
-        h_pool4_flat_1 = tf.reshape(h_pool_4, [-1, 8 * 8 * 256])
-        average = tf.nn.relu(tf.matmul(h_pool4_flat_1, w_fc_1) + b_fc_1)
+        h_pool4_flat_1 = tf.reshape(h_pool_4, [-1, 8 * 8 * 256], name='e_rfc1')
+        average = tf.nn.relu(tf.matmul(h_pool4_flat_1, w_fc_1) + b_fc_1, name='e_average')
 
         # Fully connected layer 2
         w_fc_2 = tf.get_variable('e_wfc2', [8 * 8 * 256, 256])
         b_fc_2 = tf.get_variable('e_bfc2', [256])
-        h_pool4_flat_2 = tf.reshape(h_pool_4, [-1, 8 * 8 * 256])
-        deviation = tf.nn.relu(tf.matmul(h_pool4_flat_2, w_fc_2) + b_fc_2)
+        h_pool4_flat_2 = tf.reshape(h_pool_4, [-1, 8 * 8 * 256], name='e_rfc2')
+        deviation = tf.nn.relu(tf.matmul(h_pool4_flat_2, w_fc_2) + b_fc_2, name='e_deviation')
 
     return average, deviation
 
 
-def generator(z, sample_dimension, y, batch_size, usage=''):
-    with tf.variable_scope('generator', reuse=tf.AUTO_REUSE) as scope:
+def generator(z, sample_dimension, y, batch_size):
+    with tf.variable_scope('generator', reuse=True) as scope:
         # Concatenate hair style parameter
         z = tf.concat([z, y], 1)
 
         # Fully connected layer 1
         w_fc_1 = tf.get_variable('g_wfc1', [sample_dimension + 64, 8064])
         b_fc_1 = tf.get_variable('g_bfc1', [8064])
-        h_fc_1 = tf.nn.relu(tf.matmul(z, w_fc_1) + b_fc_1)
+        h_fc_1 = tf.nn.relu(tf.matmul(z, w_fc_1) + b_fc_1, name='g_relufc1')
 
         # Fully connected layer 2
         w_fc_2 = tf.get_variable('g_wfc2', [8064, 4 * 4 * 504])
         b_fc_2 = tf.get_variable('g_bfc2', [4 * 4 * 504])
-        h_fc_2 = tf.nn.relu(tf.matmul(h_fc_1, w_fc_2) + b_fc_2)
+        h_fc_2 = tf.nn.relu(tf.matmul(h_fc_1, w_fc_2) + b_fc_2, name='g_relufc2')
 
         # Hidden Layer
-        hidden_layer = tf.reshape(h_fc_2, [-1, 4, 4, 504])
-        hidden_layer = tf.nn.relu(hidden_layer)
+        hidden_layer = tf.reshape(h_fc_2, [-1, 4, 4, 504], name='g_rh')
+        hidden_layer = tf.nn.relu(hidden_layer, name='g_reluh')
 
         # DeConv Layer 1
         w_conv1 = tf.get_variable('g_wconv1', [3, 3, 288, 504])
         b_conv1 = tf.get_variable('g_bconv1', [288])
         h_conv1 = tf.nn.conv2d_transpose(hidden_layer, w_conv1, output_shape=[batch_size, 8, 8, 288],
-                                         strides=[1, 2, 2, 1], padding='SAME') + b_conv1
-        h_conv1 = tf.contrib.layers.batch_norm(inputs=h_conv1, scale=True, scope="g_bn1")
+                                         strides=[1, 2, 2, 1], padding='SAME', name='g_tconv1') + b_conv1
+        h_conv1 = tf.contrib.layers.batch_norm(inputs=h_conv1, scale=True, scope="g_bn1", name='g_bnconv1')
         h_conv1 = tf.nn.relu(h_conv1)
 
         # DeConv Layer 2
         w_conv2 = tf.get_variable('g_wconv2', [3, 3, 216, 288])
         b_conv2 = tf.get_variable('g_bconv2', [216])
         h_conv2 = tf.nn.conv2d_transpose(h_conv1, w_conv2, output_shape=[batch_size, 16, 16, 216],
-                                         strides=[1, 2, 2, 1], padding='SAME') + b_conv2
-        h_conv2 = tf.contrib.layers.batch_norm(inputs=h_conv2, scale=True, scope="g_bn2")
+                                         strides=[1, 2, 2, 1], padding='SAME', name='g_tconv2') + b_conv2
+        h_conv2 = tf.contrib.layers.batch_norm(inputs=h_conv2, scale=True, scope="g_bn2", name='g_bnconv2')
         h_conv2 = tf.nn.relu(h_conv2)
 
         # DeConv Layer 3
         w_conv3 = tf.get_variable('g_wconv3', [5, 5, 144, 216])
         b_conv3 = tf.get_variable('g_bconv3', [144])
         h_conv3 = tf.nn.conv2d_transpose(h_conv2, w_conv3, output_shape=[batch_size, 32, 32, 144],
-                                         strides=[1, 2, 2, 1], padding='SAME') + b_conv3
-        h_conv3 = tf.contrib.layers.batch_norm(inputs=h_conv3, scale=True, scope="g_bn3")
+                                         strides=[1, 2, 2, 1], padding='SAME', name='g_tconv3') + b_conv3
+        h_conv3 = tf.contrib.layers.batch_norm(inputs=h_conv3, scale=True, scope="g_bn3", name='g_bnconv3')
         h_conv3 = tf.nn.relu(h_conv3)
 
         # DeConv Layer 4
         w_conv4 = tf.get_variable('g_wconv4', [5, 5, 72, 144])
         b_conv4 = tf.get_variable('g_bconv4', [72])
         h_conv4 = tf.nn.conv2d_transpose(h_conv3, w_conv4, output_shape=[batch_size, 64, 64, 72],
-                                         strides=[1, 2, 2, 1], padding='SAME') + b_conv4
-        h_conv4 = tf.contrib.layers.batch_norm(inputs=h_conv4, scale=True, scope="g_bn4")
+                                         strides=[1, 2, 2, 1], padding='SAME', name='g_tconv4') + b_conv4
+        h_conv4 = tf.contrib.layers.batch_norm(inputs=h_conv4, scale=True, scope="g_bn4", name='g_bnconv4')
         h_conv4 = tf.nn.relu(h_conv4)
 
         # DeConv Layer 5
         w_conv5 = tf.get_variable('g_wconv5', [6, 6, 3, 72])
         b_conv5 = tf.get_variable('g_bconv5', [3])
         h_conv5 = tf.nn.conv2d_transpose(h_conv4, w_conv5, output_shape=[batch_size, 128, 128, 3],
-                                         strides=[1, 2, 2, 1], padding='SAME') + b_conv5
+                                         strides=[1, 2, 2, 1], padding='SAME', name='g_tconv5') + b_conv5
         h_conv5 = tf.nn.tanh(h_conv5, 'g_tanh')
     return h_conv5
 
 
-def discriminator(image, usage=''):
-    with tf.variable_scope('discriminator', reuse=tf.AUTO_REUSE) as scope:
+def discriminator(image):
+    with tf.variable_scope('discriminator', reuse=True) as scope:
         # Convolution Layer 1
         w_conv1 = tf.get_variable('d_wconv1', [6, 6, 3, 64])
         b_conv1 = tf.get_variable('d_bconv1', [64])
-        h_conv1 = tf.nn.relu(conv2d(image, w_conv1, strides=[1, 1, 1, 1], padding='SAME') + b_conv1)
+        h_conv1 = tf.nn.relu(conv2d(image, w_conv1, strides=[1, 1, 1, 1], padding='SAME') + b_conv1, name='d_rconv1')
 
         # Pooling Layer 1
-        h_pool_1 = tf.layers.max_pooling2d(inputs=h_conv1, pool_size=[2, 2], strides=2)
+        h_pool_1 = tf.layers.max_pooling2d(inputs=h_conv1, pool_size=[2, 2], strides=2, name='d_pool1')
 
         # Convolution Layer 2
         w_conv2 = tf.get_variable('d_wconv2', [4, 4, 64, 128])
         b_conv2 = tf.get_variable('d_bconv2', [128])
-        h_conv2 = tf.nn.relu(conv2d(h_pool_1, w_conv2, strides=[1, 1, 1, 1], padding='SAME') + b_conv2)
+        h_conv2 = tf.nn.relu(conv2d(h_pool_1, w_conv2, strides=[1, 1, 1, 1], padding='SAME') + b_conv2, name='d_rconv2')
 
         # Pooling Layer 2
-        h_pool_2 = tf.layers.max_pooling2d(inputs=h_conv2, pool_size=[2, 2], strides=2)
+        h_pool_2 = tf.layers.max_pooling2d(inputs=h_conv2, pool_size=[2, 2], strides=2, name='d_pool2')
 
         # Convolution Layer 3
         w_conv3 = tf.get_variable('d_wconv3', [4, 4, 128, 128])
         b_conv3 = tf.get_variable('d_bconv3', [128])
-        h_conv3 = tf.nn.relu(conv2d(h_pool_2, w_conv3, strides=[1, 1, 1, 1], padding='SAME') + b_conv3)
+        h_conv3 = tf.nn.relu(conv2d(h_pool_2, w_conv3, strides=[1, 1, 1, 1], padding='SAME') + b_conv3, name='d_rconv3')
 
         # Pooling Layer 3
-        h_pool_3 = tf.layers.max_pooling2d(inputs=h_conv3, pool_size=[2, 2], strides=2)
+        h_pool_3 = tf.layers.max_pooling2d(inputs=h_conv3, pool_size=[2, 2], strides=2, name='d_pool3')
 
         # Convolution Layer 4
         w_conv4 = tf.get_variable('d_wconv4', [4, 4, 128, 256])
         b_conv4 = tf.get_variable('d_bconv4', [256])
-        h_conv4 = tf.nn.relu(conv2d(h_pool_3, w_conv4, strides=[1, 1, 1, 1], padding='SAME') + b_conv4)
+        h_conv4 = tf.nn.relu(conv2d(h_pool_3, w_conv4, strides=[1, 1, 1, 1], padding='SAME') + b_conv4, name='d_rconv4')
 
         # Pooling Layer 4
-        h_pool_4 = tf.layers.max_pooling2d(inputs=h_conv4, pool_size=[2, 2], strides=2)
+        h_pool_4 = tf.layers.max_pooling2d(inputs=h_conv4, pool_size=[2, 2], strides=2, name='d_pool4')
 
         # Fully connected layer
         w_fc = tf.get_variable('d_wfc1', [8 * 8 * 256, 1])
         b_fc = tf.get_variable('d_bfc1', [1])
-        h_pool4_flat = tf.reshape(h_pool_4, [-1, 8 * 8 * 256])
-        h_fc = tf.matmul(h_pool4_flat, w_fc) + b_fc
-    return h_fc, tf.nn.sigmoid(h_fc), h_pool_4
+        h_pool4_flat = tf.reshape(h_pool_4, [-1, 8 * 8 * 256], name='d_reshape1')
+        h_fc = tf.matmul(h_pool4_flat, w_fc, name='d_mul') + b_fc
+    return h_fc, tf.nn.sigmoid(h_fc, name='d_sigmoid'), h_pool_4
 
 
 def recognizer(discriminator, y_size, usage=''):
-    with tf.variable_scope('recognizer', reuse=tf.AUTO_REUSE) as scope:
+    with tf.variable_scope('recognizer', reuse=True) as scope:
         # Fully connected layer for y
         w_fc = tf.get_variable('r_wfc1', [8 * 8 * 256, y_size])
         b_fc = tf.get_variable('r_bfc1', [y_size])
-        h_pool4_flat = tf.reshape(discriminator, [-1, 8 * 8 * 256])
-        q_y_given_x = tf.nn.softmax(tf.matmul(h_pool4_flat, w_fc) + b_fc)
+        h_pool4_flat = tf.reshape(discriminator, [-1, 8 * 8 * 256], name='r_pool4_1')
+        q_y_given_x = tf.nn.softmax(tf.matmul(h_pool4_flat, w_fc) + b_fc, name='r_q_y_given_x')
 
         # Fully connected layer for z
         w_fc_2 = tf.get_variable('r_wfc2', [8 * 8 * 256, 256])
         b_fc_2 = tf.get_variable('r_bfc2', [256])
-        h_pool4_flat_2 = tf.reshape(discriminator, [-1, 8 * 8 * 256])
-        q_z_given_x = tf.nn.softmax(tf.matmul(h_pool4_flat_2, w_fc_2) + b_fc_2)
+        h_pool4_flat_2 = tf.reshape(discriminator, [-1, 8 * 8 * 256], name='r_pool4_2')
+        q_z_given_x = tf.nn.softmax(tf.matmul(h_pool4_flat_2, w_fc_2) + b_fc_2, name='r_q_z_given_x')
 
     return q_y_given_x, q_z_given_x
 
@@ -197,19 +197,19 @@ def train_reconstruction(batch_size, iteration_amount, epoch_amount, lambda_1=1,
 
     # Prediction outputs
     # Reconstruction
-    decoder_f = generator(z, 256, y_input, batch_size, usage='_reconstruction')
-    fake_logits, fake, discriminator_f = discriminator(decoder_f, usage='_reconstruction')
+    decoder_f = generator(z, 256, y_input, batch_size)
+    fake_logits, fake, discriminator_f = discriminator(decoder_f)
 
     # Real
-    _, _, discriminator_f_real = discriminator(image, usage='_real_image')
+    _, _, discriminator_f_real = discriminator(image)
 
     # Generated
-    decoder_f_rand = generator(z_rand_input, 256, y_input, batch_size, usage='_gen_image')
-    _, _, discriminator_f_rand = discriminator(decoder_f_rand, usage='_gen_image')
+    decoder_f_rand = generator(z_rand_input, 256, y_input, batch_size)
+    _, _, discriminator_f_rand = discriminator(decoder_f_rand)
 
     # Modified
-    decoder_f_mod = generator(z, 256, y_rand_input, batch_size, usage='_modify_image')
-    _, _, discriminator_f_mod = discriminator(decoder_f_mod, usage='_modify_image')
+    decoder_f_mod = generator(z, 256, y_rand_input, batch_size)
+    _, _, discriminator_f_mod = discriminator(decoder_f_mod)
 
     # Encoder loss
     enc_loss = encoder_loss(average, deviation, decoder_f, image, batch_size)
